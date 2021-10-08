0bbd50293f9a617b62bf707bed8b9524736bc080
Andrii <shvaika.andrii@gmail.com>
2021-10-08 15:34:01 +0300

    Fixed ZBDO farm

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -2,19 +2,19 @@ import tokens from './tokens'
 import { PoolConfig, PoolCategory } from './types'
 
 const pools: PoolConfig[] = [
-  // {
-  //   sousId: 1,
-  //   stakingToken: tokens.zbdo,
-  //   earningToken: tokens.wbnb,
-  //   contractAddress: {
-  //     97: '',
-  //     56: '0x3923E0d738fd9Ab57b429a4b3c63a2f7a0823596',
-  //   },
-  //   poolCategory: PoolCategory.CORE,
-  //   harvest: true,
-  //   tokenPerBlock: '10',
-  //   sortOrder: 1,
-  // },
+  {
+    sousId: 1,
+    stakingToken: tokens.zbdo,
+    earningToken: tokens.wbnb,
+    contractAddress: {
+      97: '0xd9145CCE52D386f254917e481eB44e9943F39138',
+      56: '0x3923E0d738fd9Ab57b429a4b3c63a2f7a0823596',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '10',
+    sortOrder: 1,
+  },
   {
     sousId: 0,
     stakingToken: tokens.cake,
